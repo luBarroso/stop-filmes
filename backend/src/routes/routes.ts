6b8709c79470370dc15b362647f3de91130fcddd
AndressaPintos <152065638+AndressaPintos@users.noreply.github.com>
2024-12-01 17:40:09 -0300

    Update routes.ts

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,14 @@
-import { getInfoFilme, getRankGenero } from "../controllers/filme.controller";
+import { getVerificaFilme, getAvalFilme, getVerificaPessoa, getRankGenero, getVotosFilme, getInfoFilme } from "../controllers/filme.controller";
 
 const { Router } = require("express");
 const router = Router();
 
 export default router;
-
+router.get("/filme/verificacao/:genero/:letra", getVerificaFilme);
+router.get("filme/avaliacao/:id", getAvalFilme);
+router.get("pessoa/verificacao/:funcao/:letra", getVerificaPessoa);
 router.get("/filme/rank/:id", getRankGenero);
+router.get("filme/votos/:id", getVotosFilme);
 router.get("/filme/:id", getInfoFilme);
 
 module.exports = router;
